7a34663c528bb760d4892f9da63a15b243c4c404
João Marques <166452577+maarqsz@users.noreply.github.com>
2024-04-08 22:01:31 -0300

    aula 08.04

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let resultado = document.getElementById('divResultado')
+console.log(resultado);
+
+// let nome = prompt("Digite seu nome: ")
+// resultado.innerHTML = "Olá, " + nome
+
+// let cont = 0
+// while(cont<10){
+//     //console.log(cont);
+//     resultado.innerHTML += cont + " patinho foi passear....<br>"
+//     cont++
+// }
+
+// while(cont>0){
+//     resultado.innerHTML += cont + " patinhos voltaram de lá....<br>"
+//     cont--
+// }
+let exercicio01 = document.getElementById('exercicio01')
+let cont = 1; cont*cont;
+while(cont<=10){
+resultado.innerHTML += cont = " numero....<br>"
+cont++
+}
+
+
